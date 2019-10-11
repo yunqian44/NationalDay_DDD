d44b8e8d9a8261d1f1e10bdd568a58f5355458f6
yunqian007 <yunqian@huodada.com>
2019-10-11 18:27:15 +0800

    新增中介总线

---
+++ NationalDay_DDD.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,24 @@
+﻿using NationalDay_DDD.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalDay_DDD.Core.Bus
+{
+    /// <summary>
+    /// 中介处理程序接口
+    /// 可以定义多个处理程序
+    /// 是异步的
+    /// </summary>
+    public interface IMediatorHandler
+    {
+        /// <summary>
+        /// 发布命令，将我们的命令模型发布到中介者模块
+        /// </summary>
+        /// <typeparam name="T"> 泛型 </typeparam>
+        /// <param name="command"> 命令模型，比如RegisterUserCommand </param>
+        /// <returns></returns>
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
---
+++ NationalDay_DDD.Infrastruct.Data/Bus/InMemoryBus.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NationalDay_DDD.Core.Bus;
+using NationalDay_DDD.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalDay_DDD.Infrastruct.Bus
+{
+    /// <summary>
+    /// 一个密封类，实现我们的中介记忆总线
+    /// </summary>
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        //构造函数注入
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// 实现我们在IMediatorHandler中定义的接口
+        /// 没有返回值
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="command">命令模型</param>
+        /// <returns></returns>
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);//这里要注意下 command 对象 command 得继承IRequest
+        }
+
+    }
+}
