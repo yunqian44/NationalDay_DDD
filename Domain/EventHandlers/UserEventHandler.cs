3de02fb113da25b175f43e1bd43b981cc2d012bc
贠乾 <yunqian8@live.com>
2019-10-15 18:12:52 +0800

    新增用户事件，用户注册事件

---
+++ Domain/EventHandlers/UserEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NationalDay_DDD.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NationalDay_DDD.Domain.EventHandlers
+{
+    /// <summary>
+    /// Student事件处理程序
+    /// 继承INotificationHandler<T>，可以同时处理多个不同的事件模型
+    /// </summary>
+    public class UserEventHandler :
+        INotificationHandler<UserRegisteredEvent>
+    {
+        // 学习被注册成功后的事件处理方法
+        public Task Handle(UserRegisteredEvent message, CancellationToken cancellationToken)
+        {
+            // 恭喜您，注册成功，欢迎加入我们。
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Domain/Events/UserRegisteredEvent.cs
@@ -0,0 +1,34 @@
+﻿using NationalDay_DDD.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NationalDay_DDD.Domain.Events
+{
+    /// <summary>
+    /// Student被添加后引发事件
+    /// 继承事件基类标识
+    /// </summary>
+    public class UserRegisteredEvent : Event
+    {
+        // 构造函数初始化，整体事件是一个值对象
+        public UserRegisteredEvent(int id, string name, string email, DateTime birthDate, string phone)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Phone = phone;
+        }
+        public int Id { get; set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public string Phone { get; private set; }
+
+    }
+}
