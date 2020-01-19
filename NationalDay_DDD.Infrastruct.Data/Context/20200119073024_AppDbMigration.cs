a77e02474c44ef59061c153382cbc2f39f8bdccb
贠乾 <yunqian8@live.com>
2020-01-19 18:19:38 +0800

    ef code first Lasting db of product add productDbContext

---
+++ NationalDay_DDD.Infrastruct.Data/Context/20200119073024_AppDbMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalDay_DDD.Infrastruct.Context
+{
+    public partial class AppDbMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", maxLength: 11, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(100)", maxLength: 20, nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Address_Province = table.Column<string>(nullable: true),
+                    Address_City = table.Column<string>(nullable: true),
+                    Address_County = table.Column<string>(nullable: true),
+                    Address_Street = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- NationalDay_DDD.Infrastruct.Data/Context/UserContext.cs
+++ NationalDay_DDD.Infrastruct.Data/Context/UserContext.cs
@@ -32,20 +32,20 @@ namespace NationalDay_DDD.Infrastruct.Data.Context
             base.OnModelCreating(modelBuilder);
         }
 
-        ///// <summary>
-        ///// 重写连接数据库
-        ///// </summary>
-        ///// <param name="optionsBuilder"></param>
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    // 从 appsetting.json 中获取配置信息
-        //    var config = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json")
-        //        .Build();
+        /// <summary>
+        /// 重写连接数据库
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 从 appsetting.json 中获取配置信息
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-        //    // 定义要使用的数据库
-        //    optionsBuilder.UseMySQL(config.GetConnectionString("DefaultConnection"));
-        //}
+            // 定义要使用的数据库
+            optionsBuilder.UseMySQL(config.GetConnectionString("DefaultConnection"));
+        }
     }
 }
