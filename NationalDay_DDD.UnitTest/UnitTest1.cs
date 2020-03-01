782e4dec8c70f94517ecfef7250d3ba458bd22a2
贠乾 <yunqian8@live.com>
2020-03-01 22:58:02 +0800

    add EFCore UnitTest Program

---
+++ NationalDay_DDD.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NationalDay_DDD.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
