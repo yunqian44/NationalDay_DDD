4949d86b0bc117afce55a3226ead46e9f7b22bfd
yunqian44 <yunqian8@live.com>
2020-03-05 18:16:56 +0800

    modifying productContext Contains AggregateRoot and Entity

--- NationalDay_DDD.Infrastruct.Data/Context/ProductContext.cs
+++ NationalDay_DDD.Infrastruct.Data/Context/ProductContext.cs
@@ -1,17 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NationalDay_DDD.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NationalDay_DDD.Infrastruct.Context
 {
-    public class ProductContext: DbContext
+    public class ProductContext : DbContext
     {
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {
 
         }
 
-        public DbSet<ProductContext> Products { get; set; }
+        //public DbSet<Product> Products { get; set; }
+
+        /// <summary>
+        /// 商品
+        /// </summary>
+        public DbSet<Product> Products
+        {
+            get { return this.Set<Product>(); }
+        }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public DbSet<Category> Categories
+        {
+            get { return this.Set<Category>(); }
+        }
+
+
+
     }
 }
