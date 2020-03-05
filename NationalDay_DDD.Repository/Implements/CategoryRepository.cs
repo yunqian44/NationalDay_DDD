c5e5b6b1ba6f9c98efe4d4671c81e458d0683555
yunqian44 <yunqian8@live.com>
2020-03-05 18:17:38 +0800

    add Category repository class

---
+++ NationalDay_DDD.Repository/Implements/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using NationalDay_DDD.Domain.Interface;
+using NationalDay_DDD.Domain.Models;
+using NationalDay_DDD.Infrastruct.Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NationalDay_DDD.Repository.Implements
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public override void GetDb()
+        {
+            Db = AbstractDbFactory.GetProductContextDbContext();
+        }
+
+
+    }
+}
